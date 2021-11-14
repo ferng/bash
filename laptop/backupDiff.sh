dc1fa7f4b1b631e2cfc49a546f16a2707ce9fada
ferng001 <ferng001@gmail.com>
2021-11-14 14:06:13 +0000

    laptop scripts

---
+++ laptop/backupDiff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+diff -qr ~/.vim ~/prog/config/unixHome/.vim
+diff -qr ~/bin ~/prog/bash/desktop
+diff -q ~/.ackrc ~/prog/config/unixHome/.ackrc
+diff -q ~/.bashrc ~/prog/config/unixHome/.bashrc
+diff -q ~/.jackdrc ~/prog/config/unixHome/.jackdrc
+diff -q ~/.pdsettings ~/prog/config/unixHome/.pdsettings
+diff -q ~/.profile ~/prog/config/unixHome/.profile
+diff -q ~/.vimrc ~/prog/config/unixHome/.vimrc
